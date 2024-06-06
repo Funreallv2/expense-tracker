5ab1a40c3ed3977945f67a3f604771fc417508aa
Funreall <rears.2002@gmail.com>
2024-06-06 23:23:34 +0300

    adde routes to backend (little change)

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
-import express from 'express';
+import express from 'express'
+import transaction from './routes/transaction.js'
 
 const PORT = process.env.PORT;
 const app = express()
 
+app.use('/api/v1/transaction', transaction)
+
 
 app.listen(PORT, ()=> {
-  console.log("Server is running...")
+  console.log(`Server is running... at port: ${PORT}`)
 })
